7e4a85a6c426990b532cf768fbc1bf1515668286
Krzysztof_Borowa <32bol4cis>
2019-03-12 10:50:26 +0100

    Beer Service
    removed Beer from CustomerService

--- beer-case/src/main/java/events/segfault/gdansk2019/stub/Beer.java
+++ beer-case/src/main/java/events/segfault/gdansk2019/stub/Beer.java
@@ -1,12 +1,11 @@
 package events.segfault.gdansk2019.stub;
 
 import lombok.Value;
-import org.joda.money.Money;
 
 @Value
 public class Beer {
 
     private String name;
     private String barcode;
-    private Money price;
+    private double price;
 }
--- beer-case/src/main/java/events/segfault/gdansk2019/stub/price/DiscountService.java
+++ beer-case/src/main/java/events/segfault/gdansk2019/stub/price/DiscountService.java
@@ -2,10 +2,6 @@ package events.segfault.gdansk2019.stub.price;
 
 import events.segfault.gdansk2019.stub.Beer;
 import lombok.NonNull;
-import org.joda.money.CurrencyUnit;
-import org.joda.money.Money;
-
-import java.math.RoundingMode;
 
 public final class DiscountService {
 
@@ -13,16 +9,16 @@ public final class DiscountService {
         throw new UnsupportedOperationException();
     }
 
-    public static Money calculateDiscount(@NonNull Beer beer, int amount) throws DiscountException {
+    public static double calculateDiscount(@NonNull Beer beer, int amount) throws DiscountException {
         if (beer.getBarcode().endsWith("1") && amount >= 4) {
             // 10% discount for beers with '1' at the end of barcode & amount >= 4
-            return beer.getPrice().multipliedBy(0.1d, RoundingMode.HALF_DOWN).multipliedBy(amount);
+            return (beer.getPrice() * 0.1d) * amount;
         } else if (beer.getBarcode().endsWith("2")) {
             // throw service exception for beers with '2' at the end of barcode
             throw new DiscountException();
         } else {
             // no discount
-            return Money.zero(CurrencyUnit.EUR);
+            return 0d;
         }
     }
 }
